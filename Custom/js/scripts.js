1250738f01bbd11b605d6aae464ef9e82878c7b1
Maccy Cheese <haydon.gunn@gmail.com>
2018-08-12 18:25:10 +1000

    Added slider for variable font display

--- Custom/js/scripts.js
+++ Custom/js/scripts.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelector("[data-js-theme-switch]").onchange = themeSwitch;
+    document.querySelector("[data-js-slider-weight]").onchange = rangeSliderWeight;
+    document.querySelector("[data-js-slider-italic]").onchange = rangeSliderItalic;
 });
 
 let bodyThemeClass = "theme--dark";
+let fontVariationWeight = 300;
+let fontVariationItalic = 0;
 
 function themeSwitch(event) {
     if (event.target.checked) {
@@ -11,4 +15,25 @@ function themeSwitch(event) {
     else {
         document.querySelector("body").classList.remove("theme--dark");
     }
+}
+
+// Range slider 
+function rangeSliderWeight(event) {
+    fontVariationWeight = event.target.value;
+
+    console.log("weight")
+    setFontVariationSettings();
+}
+
+function rangeSliderItalic(event) {
+    fontVariationItalic = event.target.value;
+
+    setFontVariationSettings();
+}
+
+function setFontVariationSettings() {
+    console.log("set method called")
+    var contentWrapper = document.querySelector("[data-js-content-wrapper]");
+    
+    contentWrapper.style.fontVariationSettings = "'wght' " + fontVariationWeight + ", 'ital' " + fontVariationItalic;
 }
