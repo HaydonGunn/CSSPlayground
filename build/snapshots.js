7744a95d3a8abf679943fc8d4d960c4f8a5e1550
HaydonGunn <haydon.gunn@gmail.com>
2020-09-06 21:43:07 +1000

    Moved snapshots file

--- build/snapshots.js
+++ build/snapshots.js
@@ -2,11 +2,11 @@ const PercyScript = require('@percy/script');
 const httpServer = require('http-server');
 
 const PORT = process.env.PORT_NUMBER || 8000;
-const TEST_URL = `http://localhost:${PORT}/build/ContrastModeTest.html`;
+const TEST_URL = `http://localhost:${PORT}/ContrastModeTest.html`;
 
 PercyScript.run(async (page, percySnapshot) => {
   let server = httpServer.createServer();
-  server.listen(PORT);
+  server.listen(PORT);  
 
   console.log(`Server started at ${TEST_URL}`);
 
